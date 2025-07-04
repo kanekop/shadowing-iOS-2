ac7ea8eb8524fb625d536ec1767de0871916c898
kanekop <yoshimune.kaneko@gmail.com>
2025-07-04 20:35:02 +0900

    Initial Commit

---
+++ ShadowingPractice2/ShadowingPractice2App.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowingPractice2App.swift
+//  ShadowingPractice2
+//
+//  Created by Apple on 2025/07/04.
+//
+
+import SwiftUI
+
+@main
+struct ShadowingPractice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShadowingPractice2Tests/ShadowingPractice2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowingPractice2Tests.swift
+//  ShadowingPractice2Tests
+//
+//  Created by Apple on 2025/07/04.
+//
+
+import Testing
+@testable import ShadowingPractice2
+
+struct ShadowingPractice2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
